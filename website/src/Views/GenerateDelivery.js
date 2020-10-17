90081d24c721b7933c441f0b710deef9af1ae215
Zeb Muller <zebmuller54@gmail.com>
2020-10-17 20:09:37 +1100

    started page

--- website/src/Views/GenerateDelivery.js
+++ website/src/Views/GenerateDelivery.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import View from '../Components/View';
+import ContentCard from '../Components/ContentCard';
 
 class GenerateDeliveries extends React.Component {
   render() {
     return (
       <View title = "Generate Deliveries">
-        
+        <ContentCard>
+          
+        </ContentCard>
       </View>
     );
   }
