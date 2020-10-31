f54043e23debd0deb200253c8a16ed0b6987f53c
Hughneerhut <cro4!ase>
2020-10-31 15:37:30 +1100

    Cleaned code + added annotations

--- Main.py
+++ Main.py
@@ -40,14 +40,12 @@ with open('datafile.csv', newline='') as csvfile:
         order = Order(row[0], row[1], row[5], row[6], row[7], row[2], row[3], row[4], row[8], row[9], row[10])
         orders.append(order)
 
+# Import ALL Australian postcodes and create objects for each containing GPS coords
 with open('australian_postcodes.csv', newline='') as csvfile:
     postcodeList = csv.reader(csvfile, delimiter=',')
     for row in postcodeList:
         postcode = Postcode(row[0], row[1], row[2])
         postcodes.append(postcode)
-
-
-total_orders = len(orders)  # total number of order
 
 # Get order object from orderID
 def get_order(orderid=""):
@@ -55,14 +53,14 @@ def get_order(orderid=""):
         if str(o.order_num) == str(orderid):
             return o
 
+#Get postcode object from postcode ID
 def get_postcode(postcode=""):
     for pc in postcodes:
         if str(pc.postcode) == str(postcode):
             return pc
 
-
+# This function returns the furthest origin from a destination. This is used to calculate a trucks starting location
 def get_starting_loc(origins = [], destination = ""):
-    #get furthest origin from destination
     d_pc = get_postcode(destination)
     furthest = 0
     furthest_pc = ""
@@ -74,8 +72,8 @@ def get_starting_loc(origins = [], destination = ""):
             furthest = dist
     return furthest_pc
 
+# This function returns the next closest source based on the last source postcode
 def get_closest(origins = [], last = "", destination = ""):
-
     last_pc = get_postcode(last)
     d_pc = get_postcode(destination)
     closest = math.sqrt((float(last_pc.lat) - float(d_pc.lat))**2 + (float(last_pc.long) - float(d_pc.long))**2)
@@ -93,50 +91,66 @@ def get_pickup_order(tuple = [], postcode = ""):
         if pc[1] == postcode:
             return pc[0]
 
+#Set current time to the time of the first record in our sample data
 current_time = pd.to_datetime("03/05/2020 08:00", dayfirst=True)
-origins = []
-destinations = []
-processed_orders = []
-new_orders = []
+
+#Initialise lists
+origins = [] #List to store origin postcodes
+destinations = [] #List to store destination postcodes
+processed_orders = [] #List to store queued orders
+
+
 cycles = 1
+total_orders = len(orders)  # total number of orders
 
-w_cap = 20000
-v_cap = 69.08
+#Set capacity thresholds based on a standard B-Double truck
+w_cap = 20000 #Max truck volume set to 20 tonnes
+v_cap = 69.08 #Max truck volume set to 69m^3
 
+#This function provides the optimisation. It is executed every 10 minutes.
 def optimise():
     global current_time
-    new_orders = []
+    new_orders = [] #List to store new orders from the last 10 minutes
     print(current_time)
+
     for o in orders:
         order_datetime = pd.to_datetime(o.ordered_date, dayfirst=True)
         #Get all orders from the last 10 minutes / 600 secs
         if (order_datetime - current_time).seconds < 600 and (order_datetime - current_time).days == 0:
             new_orders.append(o)
             processed_orders.append(o)
+            # Add new orders to MySQL database
             sql = "INSERT INTO processed (orderID, origin, destination, weight, volume, qty, status, created) VALUES (%s, %s, %s, %s, %s, %s, 'NEW', '%s')" % (o.order_num, o.from_pcode, o.to_pcode, o.weight, o.volume, o.item_qty, current_time)
             cursor.execute(sql)
             db.commit()
+
             print(o.order_num + " - Source: " + str(o.from_pcode) + ". Destination: " + str(o.to_pcode))
-            #Add records to DB
+
+            #Update the system state table
             sql = "INSERT INTO system_state (src, des, order_ID) VALUES (%s, %s, %s)"
             val = (o.from_pcode,o.to_pcode,o.order_num)
             cursor.execute(sql, val)
             db.commit()
+
             #add records to origins and destinations list if not already
             if o.to_pcode not in destinations:
                 destinations.append(o.to_pcode)
                 new_col = "d" + o.to_pcode
-                sql = "ALTER TABLE src_des_matrix ADD {} BOOLEAN".format(new_col)
+                sql = "ALTER TABLE src_des_matrix ADD %s BOOLEAN" % new_col
                 cursor.execute(sql)
                 db.commit()
+
             if o.from_pcode not in origins:
                 origins.append(o.from_pcode)
-    combos = []
+
+    combos = [] #List to store origin/destination combos.
+
     #Fill matrix and delivery tables
     cursor.execute("TRUNCATE TABLE src_des_matrix") #reset matrix table
     for src in origins:
+        #Store sources/origin postcodes in tables rows
         row = "o" + src
-        sql = "INSERT INTO src_des_matrix (src) VALUES ('{}')".format(row)
+        sql = "INSERT INTO src_des_matrix (src) VALUES ('%s')" % row
         cursor.execute(sql)
         db.commit()
         for des in destinations:
@@ -145,6 +159,7 @@ def optimise():
             for o in processed_orders:
                 if o.from_pcode == src:
                     if o.to_pcode == des:
+                        #Store destination postcodes in table columns
                         shared.append(o)
                         count = count + 1
                         col = "d" + des
@@ -174,22 +189,29 @@ def optimise():
                             val = (o.order_num, o.weight, o.volume, o.item_qty)
                             cursor.execute(sql, val)
 
-
+    #For each destination get
     for d in destinations:
+        #Get a list of sources for each destination
         sql = "SELECT %s FROM src_des_matrix" % ("d" + d)
         cursor.execute(sql)
+
         print("===== " + d)
-        i = 0
+
         combos = []
-        weight = 0 #nsw/act
+        weight = 0
         volume = 0
         sources = []
-        candidate_orders = []
+
+        candidate_orders = [] #list of possible orders for each truck
+        i = 0
+
+        #Check destination/origin matrix table to see if each source delivers to the destination
         for o in cursor.fetchall():
             if (str(o) != "(None,)"):
-                sources.append(origins[i])
+                sources.append(origins[i]) #add to sources list if record not null
                 print(origins[i])
             i = i + 1
+
         for s in sources:
             combo = "o" + s + "d" + d
             combos.append(combo)
@@ -204,7 +226,6 @@ def optimise():
                 w_cap_pct = (weight / w_cap) * 100
                 v_cap_pct = (volume / v_cap) * 100
                 ord = get_order(o[0])
-                print("TEST " + ord.order_num)
                 candidate_orders.append(ord.order_num)
 
                 if (w_cap_pct > 90) or (v_cap_pct > 90):
@@ -229,7 +250,6 @@ def optimise():
                             stops.append(next)
                             pickup_order.append((i + 1, next))
 
-                    print("LEN " + str(len(candidate_orders)))
                     for co in candidate_orders:
                         print("Order " + co)
                         cand = get_order(co)
@@ -262,24 +282,12 @@ def optimise():
                     weight = 0
                     sources = []
 
-
-
-            # if (origins[i][0] == d[0]) and (origins[i] in sources):
-            # print("same state")
-            # sql = "SELECT distance from distlocal where postcode1=%s and postcode2=%s" % (origins[i], d)
-            # cursor.execute(sql)
-            # print("DISTANCE: " + str(cursor.fetchall()))
-
-
     current_time = current_time + pd.Timedelta(seconds=600)
     new_orders.clear()
 
-#optimise is running at every 10 seconds for troubleshooting purposesxc
+#optimise is running at every 10 seconds for troubleshooting purposes
 schedule.every(5).seconds.do(optimise)
 cycles = cycles + 1
-
-
-
 
 while 1:
     schedule.run_pending()
